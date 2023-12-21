586a9391efbbac815d8dae01abeee29f55569081
Alden <aldenbe@gmail.com>
2023-12-21 05:51:22 -0800

    add aws stuff?

---
+++ utils/server-utils.ts
@@ -0,0 +1,11 @@
+// server-utils.ts
+
+import { cookies } from "next/headers";
+import { generateServerClientUsingCookies } from "@aws-amplify/adapter-nextjs/api";
+
+import config from "@/amplifyconfiguration.json";
+
+export const serverClient = generateServerClientUsingCookies({
+  config,
+  cookies,
+});
