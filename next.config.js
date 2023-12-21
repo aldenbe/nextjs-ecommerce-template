44b17d809545002bb7c96a773fdb56f78144b3af
Alden <aldenbe@gmail.com>
2023-12-21 06:07:03 -0800

    Revert "use remotepatterns instead of domains for image module"
    This reverts commit 45204b091a3b3a85e60e406502820364c74cc873.

--- next.config.js
+++ next.config.js
@@ -1,31 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'loremflickr.com',
-        port: '',
-        pathname: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placeimg.com',
-        port: '',
-        pathname: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-        port: '',
-        pathname: '',
-      },
-    ]
+    loader: "akamai",
+    domains: [
+      "picsum.photos",
+      "loremflickr.com",
+      "placeimg.com",
+      "source.unsplash.com",
+    ],
   },
 };
